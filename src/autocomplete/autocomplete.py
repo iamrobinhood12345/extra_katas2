6822fc95d915f33ad3fc3ea555792f99a9a87445
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-23 21:50:47 -0800

    autocomplete class and tests.

---
+++ src/autocomplete/autocomplete.py
@@ -0,0 +1,18 @@
+"""Module for autocomplete class."""
+
+
+class AutoCompleter(object):
+    """Autcompleter Class."""
+
+    def __init__(self, vocabulary, max_completions=5):
+        """Initialize the Autocompleter class."""
+        self._vocabulary = vocabulary
+        self._max_completions = max_completions
+
+    def complete(self, prefix):
+        """Return list of autocomplete words."""
+        words = list(set([word for word in self._vocabulary if word.startswith(prefix)]))
+        if len(words) < self._max_completions:
+            return words
+        else:
+            return words[:self._max_completions]
